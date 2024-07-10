4c06bc80e7e364032d90aca4bc1a919b69951184
SIMULATAN <simulatan2319@gmail.com>
2024-07-11 00:15:01 +0200

    Fix some dividers not showing

--- src/lib/components/flow/FlowNode.svelte
+++ src/lib/components/flow/FlowNode.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import { Handle, Position } from "@xyflow/svelte";
     import { type FlowNode, getNodeMetadata } from "$lib/model/nodes";
-    import FlowNodeSettings from "$lib/components/flow/FlowNodeSettings.svelte";
     import { example_node_metadata } from "$lib/data/example_data";
+    import FlowNodeSettings from "$lib/components/flow/FlowNodeSettings.svelte";
 
     interface Props {
         data: FlowNode
@@ -13,20 +13,19 @@
     const metadata = getNodeMetadata(example_node_metadata, data.descriptor)
 </script>
 
-
+{#if metadata?.input != null}
+    <Handle type="target" position={Position.Left}/>
+{/if}
 <div class="cursor-auto flow-node p-2 card preset-filled-surface-100-900 border-surface-200-800 divide-surface-200-800 block max-w-md divide-y-4 overflow-hidden border">
     {#if metadata}
-        {#if metadata.input != null}
-            <Handle type="target" position={Position.Left}/>
-        {/if}
-        {metadata.displayName}
+        <p>{metadata.displayName}</p>
         <div class="flow-node-options table-wrap">
             <FlowNodeSettings config={data.config}/>
         </div>
-        {#if metadata.output != null}
-            <Handle type="source" position={Position.Right}/>
-        {/if}
     {:else}
         <div class="text-center">Unknown node type</div>
     {/if}
 </div>
+{#if metadata?.output != null}
+    <Handle type="source" position={Position.Right}/>
+{/if}
