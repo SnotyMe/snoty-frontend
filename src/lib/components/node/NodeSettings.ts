352f5b8d8864d1dbf7ae3485a2c85e140df8adda
SIMULATAN <simulatan2319@gmail.com>
2024-10-08 21:07:43 +0200

    NodeSettings: fix off by one error
    classic
    
    Closes #8

--- src/lib/components/node/NodeSettings.ts
+++ src/lib/components/node/NodeSettings.ts
@@ -5,5 +5,5 @@ export function getFiltered(settings: SettingsStore, path: string[]) {
     const subobj = getRecursively(settings.settings, path) ?? {};
     const allValues = Object.entries(subobj);
     // name and type are special fields in the root settings that are shown exp
-    return allValues.filter(([key, _]) => path.length > 1 || (key !== "type" && key !== "name"))
+    return allValues.filter(([key, _]) => path.length > 0 || (key !== "type" && key !== "name"))
 }
