767cd9bdffec3f8c2abacb79ecb43fdae3de324e
SIMULATAN <simulatan2319@gmail.com>
2024-10-24 11:29:44 +0200

    Lazy load Codemirror and ElkJS to reduce Cloudflare _worker.js

---
+++ src/lib/components/node/editor/codemirror/LazyCodemirrorSettingsField.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { onMount, type SvelteComponent } from "svelte";
+
+    let CodemirrorSettingsField: null | typeof SvelteComponent;
+
+    onMount(async () => {
+        const module = await import("./CodemirrorSettingsField.svelte")
+        CodemirrorSettingsField = module.default as typeof SvelteComponent;
+    })
+</script>
+
+{#if CodemirrorSettingsField}
+    <CodemirrorSettingsField {...$$restProps}/>
+{/if}
