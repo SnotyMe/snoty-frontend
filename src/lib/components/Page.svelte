0182b577331d74e93026bbaa1a762e84fd1b4eeb
SIMULATAN <simulatan2319@gmail.com>
2024-10-31 11:40:03 +0100

    Admin: use Page component to fix page title

--- src/lib/components/Page.svelte
+++ src/lib/components/Page.svelte
@@ -1,34 +1,26 @@
 <script lang="ts">
-    import { page } from "$app/stores";
     import { PUBLIC_EXTRA_SCRIPT_URL } from "$env/static/public";
     import { env } from "$env/dynamic/public";
 
     const EXTRA_SCRIPT_URL = env.PUBLIC_EXTRA_SCRIPT_URL || PUBLIC_EXTRA_SCRIPT_URL;
 
-    let user = $page.data.user;
-
     interface Props {
         pageName: string;
-        showPageName: boolean;
-        class: string;
         children: any;
     }
 
     const {
         pageName,
-        class: classes = "",
     }: Props = $props();
-
-    let expanded = false;
 </script>
 
-<sveltekit:head>
+<svelte:head>
     {#if EXTRA_SCRIPT_URL}
         <script type="text/javascript" src={EXTRA_SCRIPT_URL} crossorigin="anonymous" defer></script>
     {/if}
     <!-- either 'pageName | Snoty' or 'Snoty' -->
     <title>{pageName ? pageName + " | " : ""}Snoty</title>
-</sveltekit:head>
+</svelte:head>
 
 <!-- svelte 5 wants {@render children()} but it is buggy, duplicating content but only if <sveltekit:head> is present -->
 <slot/>
