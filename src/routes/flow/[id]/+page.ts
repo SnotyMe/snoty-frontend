3c20f57d71100182c0c6d37256397bc8123defb9
SIMULATAN <jakob@simulatan.me>
2024-12-18 08:44:59 +0100

    Potentially fix cf illegal invocation pt.2

--- src/routes/flow/[id]/+page.ts
+++ src/routes/flow/[id]/+page.ts
@@ -2,12 +2,9 @@ import { getNodeMetadatas, getNodeTemplates } from "$lib/api/node_api";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ data, fetch }) => {
-    const metadatas = getNodeMetadatas({ fetch });
-    const templates = getNodeTemplates({ fetch });
-
     return {
         ...data,
-        metadatas,
-        templates,
+        metadatas: getNodeMetadatas({ fetch }),
+        templates: getNodeTemplates({ fetch }),
     };
 }
