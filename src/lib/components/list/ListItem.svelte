5106a3ed5ef7b750ce0285e1e2c72f15fde8a1b7
SIMULATAN <jakob@simulatan.me>
2025-04-22 22:41:30 +0200

    feat(notification): user notifications

--- src/lib/components/list/ListItem.svelte
+++ src/lib/components/list/ListItem.svelte
@@ -1,3 +1,9 @@
-<div class="card preset-filled-surface-100-900 gap-3 p-4 px-6 w-full flex justify-between items-center">
+<script>
+    import { twMerge } from "tailwind-merge";
+
+    const props = $props();
+</script>
+
+<div {...props} class={twMerge("card preset-filled-surface-100-900 gap-3 p-4 px-6 w-full flex justify-between items-center", props.class)}>
     <slot/>
 </div>
