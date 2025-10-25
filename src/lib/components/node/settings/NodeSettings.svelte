b96bd5fe4448d1df6064de6f312868293378370d
SIMULATAN <jakob@simulatan.me>
2025-10-26 00:36:11 +0200

    feat(credentials): search, select credential

--- src/lib/components/node/settings/NodeSettings.svelte
+++ src/lib/components/node/settings/NodeSettings.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import type { NodeField, SchemaValues } from "$lib/model/nodes";
-    import { COLLECTION, CREDENTIAL, MAP, OBJECT } from "$lib/model/node_field_details";
+    import { COLLECTION, MAP, OBJECT } from "$lib/model/node_field_details";
     import SettingsField from "./SettingsField.svelte";
     import PolySettings from "$lib/components/node/settings/PolySettings.svelte";
 
@@ -18,8 +18,7 @@
             {@const isList = field.type === COLLECTION}
             {@const isObject = field.type === OBJECT}
             {@const isMap = field.type === MAP}
-            {@const isCredential = field.type === CREDENTIAL}
-            {@const hasChildren = isList || isObject || isMap || isCredential}
+            {@const hasChildren = isList || isObject || isMap}
             <tr>
                 <th colspan={hasChildren ? 2 : 1}>
                     <p class="whitespace-nowrap w-[min-content]" title={field.description}>{field.displayName}</p>
